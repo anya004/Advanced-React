387e3b1b7baca507123fcfeba14d3fb46ed3c293
anya004 <annavakulyuk@gmail.com>
2019-03-09 22:19:15 -0500

    updating item backend additions

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,11 +2,7 @@ const {forwardTo} = require('prisma-binding');
 
 const Query = {
     items: forwardTo('db'),
-    // async items(parent, arg, ctx, info) {
-    //     console.log("GETTING ITEMS");
-    //     const items = await ctx.db.query.items();
-    //     return items;
-    // }
+    item: forwardTo('db'),
 };
 
 module.exports = Query;
