4b16f47fe6667635c20261934dd5f7e149e69838
anya004 <annavakulyuk@gmail.com>
2019-03-27 22:07:44 -0400

    Adds backend resolvers for signing in

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -60,6 +60,27 @@ const Mutations = {
         //return to the browser
         return user;
       },
+      async signin(parent, { email, password }, ctx, info) {
+        //1. check if there is a user with that email
+        const user = await ctx.db.query.user({ where: { email } });
+        if (!user) {
+          throw new Error(`No such user found for email ${email}`);
+        }
+        //2. check if password matches
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+          throw new Error ("Incorrect password");
+        }
+        //3. generate a JWT token
+        const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+        //4. set the cookie with the token
+        ctx.response.cookie('token', token, {
+          httpOnly: true,
+          maxAge: 1000*60*60*24*365, // 1year cookie
+        });
+        //5. return the user
+        return user;
+      },
 };
 
 module.exports = Mutations;
