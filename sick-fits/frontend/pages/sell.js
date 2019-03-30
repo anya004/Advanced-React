43502f1975a73dc34834d9669707de1ece4ee90d
anya004 <annavakulyuk@gmail.com>
2019-03-30 19:19:37 -0400

    adds wrapper component for non-signined in users

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,10 +1,13 @@
 //import React from 'react'; -- next.js imports for us
 import Link from 'next/link';
-import CreateItem from "../components/CreateItem"
+import CreateItem from "../components/CreateItem";
+import PleaseSignIn from "../components/PleaseSignIn";
 
 const Sell = props => (
   <div>
-    <CreateItem />
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 );
 
