f8f004fd8dee39ff25df217b2cab872c0a9ca3b7
anya004 <annavakulyuk@gmail.com>
2019-03-30 18:53:28 -0400

    creates a relationship between item and user

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,10 +6,17 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-        //TODO: check if user is logged in
-
+        //check if user is logged in
+        if (!ctx.request.userId) {
+          throw new Error('You must be logged in to see this page.');
+        }
         const item = await ctx.db.mutation.createItem({
             data: {
+                user: {
+                  connect: { //creating a relationship between item and user
+                    id: ctx.request.userId,
+                  },
+                },
                 ...args
             }
         }, info);
