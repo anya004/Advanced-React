49768c6571b147ce49ad0d454d1b239e8305688a
anya004 <annavakulyuk@gmail.com>
2019-03-30 22:27:05 -0400

    add middleware for adding user on the request

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -21,6 +21,17 @@ server.express.use((req, res, next)=> {
     next();
 });
 
+// create middleware that populates user on each request
+server.express.use(async (req, res, next) => {
+    //if they aren't logged in, skip
+    if (!req.userId) return next();
+    const user = await db.query.user({ where: { id: req.userId}},
+        '{ id, permissions, email, name}'
+    );
+    req.user = user;
+    next();
+});
+
 server.start({
     cors: {
         credentials: true,
