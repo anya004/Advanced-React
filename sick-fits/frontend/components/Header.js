64f1f9461be656f4367019c3d52ae27e87259d76
anya004 <annavakulyuk@gmail.com>
2019-05-16 00:47:42 -0400

    changes the name of the site

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -60,7 +60,7 @@ const Header = () => (
     <div className="bar">
       <Logo>
         <Link href="/">
-          <a>Sick Fits</a>
+          <a>BeautOn</a>
         </Link>
       </Logo>
       <Nav />
