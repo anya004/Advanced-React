73f1a8466112196615879d5fc253c9833b41814f
anya004 <annavakulyuk@gmail.com>
2019-05-16 23:26:53 -0400

    adds permission check and user signin status for editting an item

--- sick-fits/frontend/pages/update.js
+++ sick-fits/frontend/pages/update.js
@@ -1,10 +1,13 @@
 //import React from 'react'; -- next.js imports for us
 import Link from 'next/link';
 import UpdateItem from "../components/UpdateItem";
+import PleaseSignIn from "../components/PleaseSignIn";
 
 const Sell = props => (
   <div>
-    <UpdateItem id={props.query.id}/>
+    <PleaseSignIn>
+      <UpdateItem id={props.query.id}/>
+    </PleaseSignIn>
   </div>
 );
 
